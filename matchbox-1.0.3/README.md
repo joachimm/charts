064a952a58e45793379e0a40132a9c6b84485dfe
Arnaud Lemaire <alemaire@norad.fr>
2021-05-18 14:33:22 +0200

    add matchbx 1.0.3.1

---
+++ matchbox-1.0.3/README.md
@@ -0,0 +1,11 @@
+
+# matchbox
+
+This chart is a fully featured PXE server that can boot & install servers.
+Servers rootfs installed come from docker images.
+
+It contains :
+
+- dnsmasq dhcp server set up for PXE
+- matchbox set up based on chart values
+- script to automatically prepare archlinux iso as PXE boot
---
+++ matchbox-1.0.3/files/rootfs-update.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+set -x
+set -e
+
+sleepTime="{{ .Values.images.updateIntervalInSecond }}"
+images="
+{{- range .Values.matchbox.groups }}
+{{- if .metadata }}
+{{- if .metadata.dockerImage }}
+{{ .metadata.dockerImage }}
+{{- end }}
+{{- end }}
+{{- end }}
+"
+
+dockerd &
+sleep 15
+
+while true; do
+    for image in $(echo ${images} | sort | uniq); do
+        if echo ${image} | grep -q -v ":"; then
+            image="${image}:latest"
+        fi
+        imageName=${image%%:*}
+        imageCleanup=$(echo $image | tr '(/|:)' '_')
+
+        docker pull ${image}
+        id=$(docker inspect --format="{{"{{"}}.Id{{"}}"}}" ${image} | cut -f2 -d:)
+        currentId=$(readlink ${imageCleanup}.tar.gz | sed "s/${imageCleanup}-\(.*\).tar.gz/\1/")
+
+        if [ ! -f "${imageCleanup}.tar.gz" ] || [ "$currentId" != "$id" ]; then
+            echo "Updating to new version ${imageName}:${id}"
+            imageDate=$(docker inspect --format '{{"{{"}} index .Config.Labels "date"{{"}}"}}' ${id})
+            cid=$(docker run --rm -d --entrypoint=/bin/sh ${image} -c "echo 'IMAGE_HASH=\"${imageName}:${id}\"' >> /install.env; echo 'IMAGE_DATE=\"${imageDate}\"' >> /install.env; sleep 120")
+            sleep 2
+            docker export ${cid} | gzip -c > ${imageCleanup}-${id}.tar.gz
+            old=$(readlink ${imageCleanup}.tar.gz) || true
+            rm ${imageCleanup}.tar.gz || true
+            ln -s ${imageCleanup}-${id}.tar.gz ${imageCleanup}.tar.gz
+            rm -f "${old}"
+        fi
+    done
+    sleep ${sleepTime}
+done
+ 
